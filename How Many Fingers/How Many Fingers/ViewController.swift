9033f23f210d4f3e8854fa527a2bb6e248794258
Tuba Abbasi <tsabbasi@gmail.com>
2016-06-21 22:41:09 -0400

    Main view created

---
+++ How Many Fingers/How Many Fingers/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  How Many Fingers
+//
+//  Created by Tuba Abbasi on 6/21/16.
+//  Copyright © 2016 Web N App. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    @IBOutlet var userGuessTextField: UITextField!
+    
+    @IBOutlet var resultLabel: UILabel!
+    
+    
+    @IBAction func guess(sender: AnyObject) {
+        
+        let randomNumber = String(arc4random_uniform(6))
+        
+        print(randomNumber)
+        
+        if userGuessTextField == randomNumber {
+            
+            resultLabel.text = "Good Job! You are correct."
+            
+        } else {
+            
+            resultLabel.text = "Sorry, the correct answer was: \(randomNumber)"
+            
+        }
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
