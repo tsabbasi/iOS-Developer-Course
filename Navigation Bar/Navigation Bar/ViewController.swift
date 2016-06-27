2babc4a085ad8000bd3d2604965319fdb079d862
Tuba Abbasi <tsabbasi@gmail.com>
2016-06-27 15:20:58 -0400

    Basic timer completed

--- Navigation Bar/Navigation Bar/ViewController.swift
+++ Navigation Bar/Navigation Bar/ViewController.swift
@@ -9,10 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var timer = NSTimer()
+    
+    var time = 0
+    
+    
+    func increaseTimer() {
+        
+        time += 1
+        
+        timerLabel.text = ("\(time)")
+        
+    }
+    
+    @IBAction func play(sender: AnyObject) {
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("increaseTimer"), userInfo: nil, repeats: true)
+        
+    }
+    
+    @IBAction func reset(sender: AnyObject) {
+        
+        timer.invalidate()
+        
+        time = 0
+        
+        timerLabel.text = "0"
+        
+    }
+    
+    @IBAction func pause(sender: AnyObject) {
+        
+        timer.invalidate()
+        
+    }
+    @IBOutlet var timerLabel: UILabel!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
